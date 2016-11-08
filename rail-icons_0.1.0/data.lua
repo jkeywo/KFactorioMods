7aa83fecf6f390afd6d58074c888263a75ad256a
John Keyworth <john.keyworth@Rebellion.co.uk>
2016-11-08 11:30:06 +0000

    Quick rail icon mod

---
+++ rail-icons_0.1.0/data.lua
@@ -0,0 +1,5 @@
+
+data.raw["item"]["rail-signal"].icon = "__rail-icons__/rail-signal.png"
+data.raw["item"]["rail-chain-signal"].icon = "__rail-icons__/rail-chain-signal.png"
+data.raw["rail-signal"]["rail-signal"].icon = "__rail-icons__/rail-signal.png"
+data.raw["rail-chain-signal"]["rail-chain-signal"].icon = "__rail-icons__/rail-chain-signal.png"
